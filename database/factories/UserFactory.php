4caaebe232b28358f04f580dd9738c4cb1c30279
seven <751401459@qq.com>
2018-04-22 09:30:02 +0000

    Finish seeding topics and users data

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -1,6 +1,7 @@
 <?php
 
 use Faker\Generator as Faker;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +15,18 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\Models\User::class, function (Faker $faker) {
+
+    static $password;
+    $now = Carbon::now()->toDateTimeString();
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'password' => $password? : $password=bcrypt('password'), // secret
         'remember_token' => str_random(10),
+        'introduction' => $faker->sentence(),
+        'created_at' => $now,
+        'updated_at' => $now,
+
     ];
 });
