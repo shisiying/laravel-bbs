4c57c37288aa71152aca0e643ce2a740659d9ee9
seven <751401459@qq.com>
2018-04-22 08:45:36 +0000

    gererated topics

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Auth::routes();
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
