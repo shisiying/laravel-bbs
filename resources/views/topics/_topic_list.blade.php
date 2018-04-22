40cad8c61ea7e4a599e3c71117c8dea36af9ae09
seven <751401459@qq.com>
2018-04-22 11:22:11 +0000

    finish topic page

---
+++ resources/views/topics/_topic_list.blade.php
@@ -0,0 +1,51 @@
+@if(count($topics))
+
+    <ul class="media-list">
+        @foreach($topics as $topic)
+            <li class="media">
+
+                <div class="media-left">
+                    <a href="{{route('users.show',[$topic->user_id])}}"></a>
+                    <img class="media-object img-thumbnail" style="width: 52px; height: 52px;" src="{{ $topic->user->avatar }}" title="{{ $topic->user->name }}">
+                </div>
+
+                <div class="media-body">
+
+                    <div class="media-heading">
+
+                         <a href="{{ route('topics.show', [$topic->id]) }}" title="{{ $topic->title }}">
+                                {{ $topic->title }}
+                         </a>
+
+                        <a class="pull-right" href="{{ route('topics.show', [$topic->id]) }}" >
+                            <span class="badge"> {{ $topic->reply_count }} </span>
+                        </a>
+
+                    </div>
+
+                    <div class="media-body meta">
+
+                       <a href="#" title="{{ $topic->category->name }}">
+                            <span class="glyphicon glyphicon-folder-open" aria-hidden="true"></span>
+                             {{ $topic->category->name }}
+                        </a>
+
+                         <span> • </span>
+                            <a href="{{ route('users.show', [$topic->user_id]) }}" title="{{ $topic->user->name }}">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                {{ $topic->user->name }}
+                            </a>
+                            <span> • </span>
+                            <span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+                            <span class="timeago" title="最后活跃于">{{ $topic->updated_at->diffForHumans() }}</span>
+                        </div>
+
+                </div>
+
+            </li>
+        @endforeach
+    </ul>
+@else
+    <div class="empty-block">暂无数据 ~_~ </div>
+
+@endif
