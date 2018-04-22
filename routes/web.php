d0dfecdb6630efb30645a17d3bacf978be3ce530
seven <751401459@qq.com>
2018-04-22 11:44:01 +0000

    category topic list

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::resource('users','UsersController',['only'=>['show','update','edit']]);
 
 
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
