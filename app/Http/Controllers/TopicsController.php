83efe47364072ad4e40c78ce14bd369650bc672c
seven <751401459@qq.com>
2018-04-23 08:26:02 +0000

    finish topic edit

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -47,7 +47,9 @@ class TopicsController extends Controller
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+        $this->authorize('update', $topic);
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
