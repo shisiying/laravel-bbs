7369af9c164e0356ba303cac4707ad9b6163120b
seven <751401459@qq.com>
2018-04-23 08:18:00 +0000

    prevent xss injection

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -11,6 +11,7 @@ class TopicObserver
 {
     public function saving(Topic $topic)
     {
+        $topic->body = clean($topic->body,'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 
