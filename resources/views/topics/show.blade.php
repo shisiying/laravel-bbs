b7a395d0a521f7163814ec75c02fe309e96fc259
seven <751401459@qq.com>
2018-04-27 03:44:11 +0000

    add laravel-permission package

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -7,23 +7,6 @@
 
 <div class="row">
 
-    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
-        <div class="panel panel-default">
-            <div class="panel-body">
-                <div class="text-center">
-                    作者：{{ $topic->user->name }}
-                </div>
-                <hr>
-                <div class="media">
-                    <div align="center">
-                        <a href="{{ route('users.show', $topic->user->id) }}">
-                            <img class="thumbnail img-responsive" src="{{ $topic->user->avatar }}" width="300px" height="300px">
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
 
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
         <div class="panel panel-default">
@@ -61,13 +44,40 @@
                 @endcan
             </div>
         </div>
+
+
         <div class="panel panel-default topic-reply">
             <div class="panel-body">
-                @includeWhen(Auth::check(),'topics._reply_box',['topic'=>$topic])
                 @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
              </div>
         </div>
 
+        <div class="panel panel-default topic-reply">
+            <div class="panel-body">
+                @includeWhen(Auth::check(),'topics._reply_box',['topic'=>$topic])
+             </div>
+        </div>
+
     </div>
+
+    <div class="col-lg-3 col-md-3 hidden-sm hidden-xs author-info">
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="text-center">
+                    作者：{{ $topic->user->name }}
+                </div>
+                <hr>
+                <div class="media">
+                    <div align="center">
+                        <a href="{{ route('users.show', $topic->user->id) }}">
+                            <img class="thumbnail img-responsive" src="{{ $topic->user->avatar }}" width="300px" height="300px">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 </div>
 @stop
