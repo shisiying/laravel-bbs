b1a1595c917a277c91a745913c985dccfae6e2ab
seven <751401459@qq.com>
2018-05-03 12:13:40 +0000

    增加了后台管理模块--内容管理模块，用户管理模块

--- config/administrator.php
+++ config/administrator.php
@@ -59,8 +59,15 @@ return array(
      *	)
      */
     'menu' => [
-        '用户与权限'=>[
+        '用户角色与权限'=>[
             'users',
+            'roles',
+            'permissions',
+        ],
+        '内容管理'=>[
+            'categories',
+            'topics',
+            'replies',
         ],
     ],
 
