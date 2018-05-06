bf69119ad0b1326369d5fd2fbc1b7ee3a4306fa0
seven <751401459@qq.com>
2018-05-06 13:35:30 +0000

    增加了用户默认头像功能

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 
-Route::get('/','PagesController@root')->name('root');
+Route::get('/','TopicsController@index')->name('root');
 Auth::routes();
 
 Route::resource('users','UsersController',['only'=>['show','update','edit']]);
